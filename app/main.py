a67d1ff60ce86fec3e7e6d77e4b1b121bddeed4e
Antonov Andrew <prestigemilitary@gmail.com>
2025-05-01 22:52:59 +0300

    maybe not yet

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ class Dog(Animal):
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list[Animal]) -> None:
+def feed_animals(animals: list[Animal]) -> int:
     total_points = 0
     for animal in animals:
         total_points += animal.feed()
